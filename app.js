a5c6dac31d25dad31c094d8dc0d6cbeb7002b2ce
Akshithabollampally <102281539+Akshithabollampally@users.noreply.github.com>
2023-06-21 22:36:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,80 +4,157 @@ if (process.env.NODE_ENV !== "production") {
 
 
 const express = require('express');
-const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override')
-
-
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 const Product = require('./models/product');
+const {isLoggedIn}=require('./middleware');
 
-mongoose.set('strictQuery', true);
+
+const userRoutes = require('./routes/users');
 
 
 const dbUrl = process.env.MONGODB_URI
+// process.env.DB_URL
 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Continue with your code
-  })
-  .catch(error => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+// 'mongodb://127.0.0.1:27017/STUDENTS'
+mongoose.connect(dbUrl)
+.then(()=>{console.log("Mongoose Connection open!!!")})
+.catch(err=>{
+    console.log('Oh No!! mongoose connection error!!');
+    console.log(err)
+});
 
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
+
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname + '/public/'));
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
 
-app.get('/products', async (req, res) => {
-    
-        const products = await Product.find({  })
-        res.render('products/index', { products })
-   
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 })
 
-app.get('/products/new', (req, res) => {
-    res.render('products/new')
-})
+
+app.use('/', userRoutes);
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/products',isLoggedIn, async (req, res) => {
+
+    try {
+        const userId = req.user._id;
+        const products = await Product.find({ userId: userId }).exec();
+        res.render('products/index', { products: products, user: req.user });
+      } catch (err) {
+        console.log(err);
+        res.status(500).send('An error occurred');
+      }
+
+    // const { title } = req.query;
+    // if (title) {
+    //     const userId = req.user._id;
+    //     const products = await Product.find( { title })
+    //     res.render('products/index', { products, title })
+    // } else {
+    //     const products = await Product.find({})
+    //     res.render('products/index', { products })
+    // }
+});
+
+app.get('/products/new',isLoggedIn, (req, res) => {
+    const userId = req.user._id;
+    res.render('products/new', { user: req.user });
+});
 
 app.post('/products', async (req, res) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`)
-})
+});
 
-app.get('/products/:id', async (req, res) => {
+app.get('/products/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
     res.render('products/show', { product })
-})
+});
 
-app.get('/products/:id/edit', async (req, res) => {
+app.get('/products/:id/edit',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product })
-})
+    res.render('products/edit', { product });
+});
 
-app.put('/products/:id', async (req, res) => {
+app.put('/products/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/products/${product._id}`);
-})
+});
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
-    const deletedProduct = await Product.findByIdAndDelete(id);
+    await Product.findByIdAndDelete(id);
     res.redirect('/products');
+});
+
+
+
+
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
-
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
 app.listen(3000, () => {
-    console.log("APP IS LISTENING ON PORT 3000!")
+    console.log('Serving on port 3000')
 })
 
 
